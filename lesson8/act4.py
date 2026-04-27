0d6039bc00d0acaa605232a19592ee9d9714c6bf
aavyankumar-hash <aavyan.kumar@gmail.com>
2026-04-27 21:10:40 +0100

    act4.py

---
+++ lesson8/act4.py
@@ -0,0 +1,13 @@
+a = int(input("enter value 1"))
+b = int(input("enter value 2"))
+c = int(input("enter value 3"))
+
+avg=(a+b+c)/3
+print("YOUR MEAN is ONLY:",avg)
+
+if a < avg:
+    print(" average is greater than A")
+if b < avg:
+    print("average is greater than ")
+if c < avg:
+    print("average is greater than c")    
